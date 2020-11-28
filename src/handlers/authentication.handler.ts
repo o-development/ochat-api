8d5b492768a6e03d5711533881200f2d0609d917
jaxoncreed <jaxoncreed@gmail.com>
2020-11-27 22:57:36 -0500

    Changed web browser auth to be cookie based

--- src/handlers/authentication.handler.ts
+++ src/handlers/authentication.handler.ts
@@ -4,17 +4,26 @@ import URL from "url-parse";
 import { sessionManager, setSessionByWebId } from "../util/AuthSessionManager";
 
 const hostUrl = process.env.HOST_URL || "http://localhost:9000";
+const clientOrigin = process.env.CLIENT_ORIGIN || "http://localhost:19006";
+const sessionSecret = process.env.SESSION_SECRET;
+const env = process.env.ENV;
+
+if (!sessionSecret) {
+  throw new Error("SESSION_SECERET must be provided");
+}
 
 const authenticationHandler: IHandler = (app) => {
   app.use(
     "/auth",
     session({
-      secret: "I let Kevin's son beat me in foosball",
-      cookie: { secure: true },
+      secret: sessionSecret,
+      cookie: { secure: env !== "dev" },
     })
   );
 
   app.get("/auth/login", async (req, res) => {
+    console.log("LOGIN");
+    console.log(req.sessionID);
     const { redirect, issuer } = req.query;
 
     const session = await sessionManager.getSession(req.sessionID);
@@ -31,20 +40,30 @@ const authenticationHandler: IHandler = (app) => {
   });
 
   app.get("/auth/callback", async (req, res) => {
+    console.log("CALLBACK");
+    console.log(req.sessionID);
     await sessionManager.handleIncomingRedirect(req.url);
     const authSession = await sessionManager.getSession(req.sessionID);
+    console.log(authSession);
     await setSessionByWebId(authSession);
     if (req.session && req.session.redirect) {
-      res.redirect(
-        `${req.session.redirect}?key=${req.sessionID}&webid=${authSession.info.webId}`
-      );
+      if (new URL(req.session.redirect).origin === clientOrigin) {
+        // If the request is coming from the client, set a cookie
+        res.cookie("auth", req.sessionID, { httpOnly: true });
+        res.redirect(`${req.session.redirect}?webid=${authSession.info.webId}`);
+      } else {
+        // otherwise send it in the redirect
+        res.redirect(
+          `${req.session.redirect}?key=${req.sessionID}&webid=${authSession.info.webId}`
+        );
+      }
     } else {
       res.status(500).send("Session problem");
     }
   });
 
   app.use(async (req, res, next) => {
-    const authSessionId = req.headers.authorization;
+    const authSessionId = req.headers.authorization || req.cookies.auth;
     if (!authSessionId) {
       next();
       return;
--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,23 @@ import handlers from "./handlers/handlers";
 import cronJobs from "./cronJobs/cronJobs";
 import startupJobs from "./startupJobs/startupJobs";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const env = process.env.ENV;
+const clientOrigin = process.env.CLIENT_ORIGIN;
 
 async function run() {
   const PORT = process.env.PORT || 9000;
 
   const app = express();
-  app.use(cors());
+  app.use(
+    cors({
+      origin: clientOrigin,
+      credentials: true,
+    })
+  );
   app.use(bodyParser.json());
+  app.use(cookieParser());
 
   app.get("/", (req, res) => {
     res.send("API Online.");
@@ -23,8 +33,10 @@ async function run() {
 
   cronJobs.forEach((cronJob) => cronJob());
 
-  console.log("Running startup jobs");
-  await Promise.all(startupJobs.map((startupJob) => startupJob()));
+  if (env !== "dev") {
+    console.log("Running startup jobs");
+    await Promise.all(startupJobs.map((startupJob) => startupJob()));
+  }
 
   app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 }
